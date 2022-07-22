3f1b3bff43c890b0dc02ef5199343025c032f9e4
neodin2015 <neodin2015@gmail.com>
2022-07-22 13:04:46 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <circle-diagram/>
+  </div>
+</template>
+
+<script>
+import circleDiagram from './components/circleDiagram.vue'
+export default {
+  name: 'App',
+  components:{
+    circleDiagram
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/circleDiagram.vue
@@ -0,0 +1,33 @@
+<template>
+  <Doughnut :chart-options="chartOptions" :chart-data="chartData" :width="50" />
+</template>
+
+<script>
+import { Doughnut } from "vue-chartjs";
+import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
+
+ChartJS.register(Tooltip, Legend, ArcElement);
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+};
+export default {
+  name: "BarChart",
+  components: { Doughnut },
+  data() {
+    return {
+      chartOptions,
+      chartData: {
+        labels: ["January", "February", "March"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#f87979", "#000"],
+            data: [40, 20, 12],
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
